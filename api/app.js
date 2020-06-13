2ed03164c31359534a85ff42980fde32243f2956
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-13 20:37:16 +0100

    added morgan middleware to print requests to server console

--- api/app.js
+++ api/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
     res.status(200).json({
