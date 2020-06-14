91cd7ce96962fe1fd5da581633ceb5834b27fcee
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-14 08:08:24 +0100

    added mongoose and body parser node packages and tested body parser with reqest and response

--- api/app.js
+++ api/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
+app.use('/applications', (req, res, next) => {
+    const name = req.body.name;
     res.status(200).json({
-        message: "run'n!"
+        message: "run'n!",
+        name: name
     })
 });
 
