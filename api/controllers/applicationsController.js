6af42a2af076af64dac85450e4e6f7f92a21bd88
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-15 09:23:36 +0100

    created a GET all applications route with basic error handling

--- api/controllers/applicationsController.js
+++ api/controllers/applicationsController.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose');
 const Application = require('../models/applicationModel')
 
+exports.get_all_applications = (req, res, next) => {
+    Application
+        .find()
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                application: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        jobTitle: doc.jobTitle
+                    }
+                })
+            }
+
+            if (docs.length > 0) {
+                res.status(200).json({response})
+            } else {
+                res.status(404).json({
+                    message: 'no data in db'
+                })
+            }
+
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        })
+};
+
 exports.log_application = (req, res, next) => {
     const application = new Application ({
         _id: new mongoose.Types.ObjectId(),
