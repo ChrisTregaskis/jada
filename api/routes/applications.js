29f226684f2a0382c797a93f47480500889b185e
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-16 17:41:34 +0100

    added delete application route

--- api/routes/applications.js
+++ api/routes/applications.js
@@ -5,5 +5,6 @@ const applicationsController = require('../controllers/applicationsController');
 router.get('/', applicationsController.get_all_applications)
 router.post('/', applicationsController.log_application)
 router.get('/:applicationId', applicationsController.get_application)
+router.delete('/:applicationId', applicationsController.delete_application)
 
 module.exports = router;
