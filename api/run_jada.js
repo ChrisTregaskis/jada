72e92fdf98499529a8c5e5b07495f93d267ee19c
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-17 19:03:02 +0100

    refactored entering search params from logged in job seeker search boxes

--- api/run_jada.js
+++ api/run_jada.js
@@ -1,7 +1,16 @@
 const WebDriver = require('selenium-webdriver');
 const driver = new WebDriver.Builder().forBrowser('chrome').build();
+const jobTitle = 'software developer';
+const area = 'Bath';
+const radius = 30;
 
+run_jada(jobTitle, area, radius);
+
+// procedural JADA function
 async function run_jada(jobTitle, area, radius) {
+    const radiusOptions = [0, 5, 10, 20, 30];
+    let radiusValid = radiusOptions.includes(radius);
+    if (!radiusValid) { return console.log('SESSION FAILED: invalid radius option') }
 
     await navigate_to_website();
     let navigateToLogin = await navigate_to_loginPage();
@@ -10,30 +19,11 @@ async function run_jada(jobTitle, area, radius) {
     let login = await jobSeeker_login();
     if (!login) { return console.log('SESSION FAILED: logged in user search button not found') }
 
-
-
-    await driver.findElement({ id: 'keywords' }).sendKeys(jobTitle);
-    await driver.findElement({ id: 'location' }).sendKeys(area);
-    await driver.findElement({ id: 'LocationType' }).sendKeys(radius);
-    console.log('successfully entered job search parameters')
-    await driver.findElement({ id: 'search-button' }).click();
-
-    let resultsPage = await driver.wait(WebDriver.until.elementLocated({ xpath: '//*[@id="90250477"]/div/div/div[1]/a'}), 2000);
-    if (resultsPage) {
-        console.log('successfully reached results page');
-    } else {
-        return console.log('first result not found');
-    }
-
-
-
+    let enteredSearch = enter_search(jobTitle, area, radius);
+    if (!enteredSearch) { return console.log('SESSION FAILED: first xpath result not found') }
 }
 
-// need to check that radius is only 0, 5, 10, 20, 30
-run_jada('software developer', 'Bath', 20)
-
 // fun fun functions
-
 async function navigate_to_website() {
     const url = 'https://www.totaljobs.com/';
     await driver.get(url);
@@ -84,5 +74,21 @@ async function jobSeeker_login() {
         return true
     } else {
         return false
+    }
+}
+
+async function enter_search(jobTitle, area, radius) {
+    await driver.findElement({ id: 'keywords' }).sendKeys(jobTitle);
+    await driver.findElement({ id: 'location' }).sendKeys(area);
+    await driver.findElement({ id: 'LocationType' }).sendKeys(radius);
+    console.log('successfully entered job search parameters')
+    await driver.findElement({ id: 'search-button' }).click();
+
+    let resultsPage = await driver.wait(WebDriver.until.elementLocated({ xpath: '//*[@id="90250477"]/div/div/div[1]/a'}), 2000);
+    if (resultsPage) {
+        console.log('successfully reached results page');
+        return true;
+    } else {
+        return false;
     }
 }
