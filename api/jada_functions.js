d70bd6e01ad494868c37e966a9e0344557d1efff
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-18 14:12:49 +0100

    successfully grabbing result job ids. This commit is for TESTING results page

--- api/jada_functions.js
+++ api/jada_functions.js
@@ -2,19 +2,20 @@ const WebDriver = require('selenium-webdriver');
 const driver = new WebDriver.Builder().forBrowser('chrome').build();
 
 exports.navigate_to_website = async function() {
-    const url = 'https://www.totaljobs.com/';
-    await driver.get(url);
-    driver.getTitle()
-        .then(title => {
-            if (title === 'Jobs | UK Job Search | Find your perfect job - totaljobs') {
-                console.log(`successfully navigated to ${url}`);
-            } else {
-                console.log('page title does not match expected url page title')
-            }
-        })
-        .catch(err => {
-            console.log(err)
-        });
+    driver.get(`https://www.totaljobs.com/jobs/software-engineer/in-bath?radius=20&s=header`)
+    // const url = 'https://www.totaljobs.com/';
+    // await driver.get(url);
+    // driver.getTitle()
+    //     .then(title => {
+    //         if (title === 'Jobs | UK Job Search | Find your perfect job - totaljobs') {
+    //             console.log(`successfully navigated to ${url}`);
+    //         } else {
+    //             console.log('page title does not match expected url page title')
+    //         }
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //     });
 };
 
 exports.navigate_to_loginPage = async function() {
@@ -61,7 +62,7 @@ exports.enter_search = async function(jobTitle, area, radius) {
     console.log('successfully entered job search parameters')
     await driver.findElement({ id: 'search-button' }).click();
 
-    let resultsPage = await driver.wait(WebDriver.until.elementLocated({ xpath: '//*[@id="90250477"]/div/div/div[1]/a'}), 2000);
+    let resultsPage = await driver.wait(WebDriver.until.elementLocated({ xpath: '//*[@id="scroll-to-top"]'}), 2000);
     if (resultsPage) {
         console.log('successfully reached results page');
         return true;
@@ -69,3 +70,49 @@ exports.enter_search = async function(jobTitle, area, radius) {
         return false;
     }
 };
+
+exports.populate_potential_jobs = async function() {
+    let additionalJobs = [];
+    let jobs = await driver.findElements({ className: 'job' })
+    for (i = 0; i < jobs.length; i++) {
+        let currentJobWebElement = await jobs[i];
+        let currentJobId = await currentJobWebElement.getAttribute('id');
+        additionalJobs.push(currentJobId);
+    }
+    return additionalJobs;
+}
+
+// exports.interested = async function(viewedResults, dkw, udkw) {
+//
+// };
+
+exports.test = async function() {
+    let additionalJobs = [];
+    let resultsPage = await driver.wait(WebDriver.until.elementLocated({ xpath: '//*[@id="scroll-to-top"]'}), 2000);
+    if (resultsPage) {
+        console.log('successfully reached results page');
+    } else {
+        return console.log('SESSION ERROR: scroll top element not found');
+    }
+
+    //all job web elements
+    let jobs = await driver.findElements({ className: 'job' });
+    let job2WE = await jobs[1];
+    let job2Id = await job2WE.getAttribute('id');
+
+    //single job web element
+    let jobWE = await driver.findElement({ css: '.job' });
+    let jobId = await jobWE.getAttribute('id');
+
+    console.log(jobId)
+    console.log(job2Id)
+
+    //take jobs arr length and loop through, grabbing each element and awaiting id attribute to add to additional jobs array
+    for (i = 0; i < jobs.length; i++) {
+        let currentJobWebElement = await jobs[i];
+        let currentJobId = await currentJobWebElement.getAttribute('id');
+        additionalJobs.push(currentJobId);
+    }
+
+    console.log(additionalJobs)
+}
--- api/run_jada.js
+++ api/run_jada.js
@@ -1,4 +1,7 @@
 const Jada = require('./jada_functions');
+const dkw = ['software', 'engineer', 'engineering', 'developer', 'full stack', 'Git', 'bash', 'node', 'agile',
+    'backend', 'frontend', 'Php', 'OOP', 'JavaScript', 'JS', 'HTML', 'CSS', 'MySQL', 'mongoDB', 'RESTful', 'API'];
+const udkw = ['Traineeship', '.NET', 'Trainee', 'consultant', 'UX', 'designer', 'sales'];
 const jobTitle = 'software developer';
 const area = 'Bath';
 const radius = 30;
@@ -7,17 +10,24 @@ run_jada(jobTitle, area, radius);
 
 async function run_jada(jobTitle, area, radius) {
     const radiusOptions = [0, 5, 10, 20, 30];
+    let viewedResults = [];
+    let potentialJobsIds = [];
     let radiusValid = radiusOptions.includes(radius);
     if (!radiusValid) { return console.log('SESSION FAILED: invalid radius option') }
 
     await Jada.navigate_to_website();
-    let navigateToLogin = await Jada.navigate_to_loginPage();
-    if (!navigateToLogin) { return console.log('SESSION FAILED: navigate to login page failed') }
+    // let navigateToLogin = await Jada.navigate_to_loginPage();
+    // if (!navigateToLogin) { return console.log('SESSION FAILED: navigate to login page failed') }
+    //
+    // let login = await Jada.jobSeeker_login();
+    // if (!login) { return console.log('SESSION FAILED: logged in user search button not found') }
+    //
+    // let enteredSearch = await Jada.enter_search(jobTitle, area, radius);
+    // if (!enteredSearch) { return console.log('SESSION FAILED: first xpath result not found') }
+    //
+    // let additionalJobs = await Jada.populate_potential_jobs();
+    // potentialJobsIds.push(additionalJobs)
+    // console.log(additionalJobs);
 
-    let login = await Jada.jobSeeker_login();
-    if (!login) { return console.log('SESSION FAILED: logged in user search button not found') }
-
-    let enteredSearch = Jada.enter_search(jobTitle, area, radius);
-    if (!enteredSearch) { return console.log('SESSION FAILED: first xpath result not found') }
-
+    await Jada.test();
 }
