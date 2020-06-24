998c0b4ff16039271ccf2f3ebdc0b29f341129e8
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-24 21:56:25 +0100

    added a route that takes a session id and returns all docs with that id associated

--- api/controllers/applicationsController.js
+++ api/controllers/applicationsController.js
@@ -51,6 +51,57 @@ exports.get_all_applications = (req, res, next) => {
         })
 };
 
+exports.get_by_session_id = (req, res, next) => {
+    let sessionId = req.params.sessionId
+    Application
+        .find({ "session_id" : sessionId })
+        .exec()
+        .then(docs => {
+            const response = {
+                status: 200,
+                count: docs.length,
+                applications: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        session_id: doc.session_id,
+                        session_date: doc.session_date,
+                        session_time: doc.session_time,
+                        job_title: doc.job_title,
+                        totalJobs_id: doc.totalJobs_id,
+                        apply_attempted: doc.apply_attempted,
+                        interested: doc.interested,
+                        salary: doc.salary,
+                        company: doc.company,
+                        job_type: doc.job_type,
+                        job_posted: doc.job_posted,
+                        location: doc.location,
+                        job_url: doc.job_url,
+                        job_contact: doc.job_contact,
+                        totalJobs_ref: doc.totalJobs_ref,
+                        found_dkw: doc.found_dkw,
+                        found_udkw: doc.found_udkw,
+                        found_top24: doc.found_top24
+
+                    }
+                })
+            }
+
+            if (docs.length > 0) {
+                res.status(200).json({response})
+            } else {
+                res.status(404).json({
+                    message: 'no data in db'
+                })
+            }
+
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        })
+};
+
 exports.log_application = (req, res, next) => {
     const application = new Application ({
         _id: new mongoose.Types.ObjectId(),
