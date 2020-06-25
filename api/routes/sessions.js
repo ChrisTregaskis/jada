5b9ff0ef25dd71982584a1f2cc03f42d833a9896
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-06-25 10:42:09 +0100

    added GET route for getting ALL sessions

--- api/routes/sessions.js
+++ api/routes/sessions.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const sessionsController = require('../controllers/sessionsController');
 
+router.get('/', sessionsController.get_all_sessions);
 router.post('/', sessionsController.log_session);
 
 module.exports = router;
