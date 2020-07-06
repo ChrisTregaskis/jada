ae05392ff68e282f8a97cfb8b0f9e18bff7ebe6e
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-06 12:53:51 +0100

    getting dynamic greeting working

--- jada-app/src/Components/PageHeader/PageHeader.js
+++ jada-app/src/Components/PageHeader/PageHeader.js
@@ -2,12 +2,40 @@ import React from "react";
 import './pageHeader.css';
 
 class PageHeader extends React.Component {
-    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentTime: '',
+            greetingMsg: 'Hello '
+        }
+    }
+
+    componentDidMount() {
+        this.getTime();
+    }
+
+    getTime = () => {
+        let today = new Date();
+        let hh = today.getHours();
+
+        if (hh < 11) {
+            this.setState({ greetingMsg: 'Good Morning ' })
+        } else if (hh > 11 && hh < 18) {
+            this.setState({ greetingMsg: 'Good Afternoon '})
+        } else if (hh > 17) {
+            this.setState({ greetingMsg: 'Good Evening '})
+        } else {
+            this.setState({ greetingMsg: 'Yo '})
+        }
+
+    }
 
     render() {
+        let greeting = this.state.greetingMsg + 'Chris';
         return (
             <div>
-                <h1>Yo yo yippi yi yay</h1>
+                <h1>{greeting}</h1>
             </div>
         )
     }
