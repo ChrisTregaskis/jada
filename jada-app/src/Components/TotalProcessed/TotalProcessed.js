21eb0acaef19ed9d4bd2fbe90075d35e4d685b2c
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-07 16:09:05 +0100

    updated total processed to be accuate dynamic figure

--- jada-app/src/Components/TotalProcessed/TotalProcessed.js
+++ jada-app/src/Components/TotalProcessed/TotalProcessed.js
@@ -1,14 +1,31 @@
 import React from "react";
 import './totalProcessed.css';
+import CountUp from "react-countup";
 
 class TotalProcessed extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            applicationsCount: 0
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.applications !== this.props.applications) {
+            this.setState({ applicationsCount: this.props.applications.length })
+        }
+    }
+
 
     render() {
         return (
             <div className="totalProcessedBox">
                 <p className="totalProcessedTitle">TOTAL</p>
                 <p className="totalProcessedTitle">PROCESSED:</p>
-                <p className="totalProcessedFigure">307</p>
+                <p className="totalProcessedFigure">
+                    <CountUp end={this.state.applicationsCount}/>
+                </p>
             </div>
         )
     }
