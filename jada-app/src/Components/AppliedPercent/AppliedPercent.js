d042b8bfb502cdfcd935462a33e6ca262d181726
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-08 11:54:45 +0100

    created file structure for applied percent component

---
+++ jada-app/src/Components/AppliedPercent/AppliedPercent.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class AppliedPercent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default AppliedPercent;
