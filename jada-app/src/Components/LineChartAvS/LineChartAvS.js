ddc86c7602eabe4046f1e87f2935e22486f4b16e
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-08 10:44:10 +0100

    renamed LineChartAvS to LineChartAvSConstructor and refactored AvS line chart code into new component LineChartAvS and calling in MainDashBoard

--- jada-app/src/Components/LineChartAvS/LineChartAvS.js
+++ jada-app/src/Components/LineChartAvS/LineChartAvS.js
@@ -1,73 +1,112 @@
 import React from "react";
-import Chart from 'chart.js';
 import './lineChartAvS.css';
+import LineChartAvSConstructor from "../LineChartAvSConstructor/LineChartAvSConstructor";
 
 class LineChartAvS extends React.Component {
     constructor(props) {
         super(props);
-        this.chartRef = React.createRef();
-    }
 
-    componentDidMount() {
-        this.produceChart()
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevProps.data !== this.props.data) {
-            this.myChart.destroy();
-            this.produceChart();
+        this.state = {
+            applications: {},
+            sessionDates:[],
+            avsData:[{
+                title: {
+                    applied: 'Applied',
+                    skipped: 'Skipped'
+                },
+                data: {
+                    applied: [],
+                    skipped: []
+                }
+            }]
         }
     }
 
-    produceChart = () => {
-        this.myChart = new Chart (this.chartRef.current, {
-            type: 'line',
-            options: {
-                scales: {
-                    xAxes: [
-                        {
-                            type: 'time',
-                            time: {
-                                unit: 'day'
-                            }
-                        }
-                    ],
-                    yAxes: [
-                        {
-                            ticks: {
-                                min: 0
-                            }
-                        }
-                    ]
-                }
-            },
-            data: {
-                labels: this.props.data.applied.map(d => d.time),
-                datasets: [{
-                    label: this.props.title.applied,
-                    data: this.props.data.applied.map(d => d.value),
-                    fill: 'none',
-                    backgroundColor: '#2ecc71',
-                    pointRadius: 2,
-                    borderColor: '#2ecc71',
-                    borderWidth: 1
-                },
-                    {
-                        label: this.props.title.skipped,
-                        data: this.props.data.skipped.map(d => d.value),
-                        fill: 'none',
-                        backgroundColor: '#e74c3c',
-                        pointRadius: 2,
-                        borderColor: '#e74c3c',
-                        borderWidth: 1
-                    }]
-            }
+    componentDidMount() {
+        this.setState({
+            applications: this.props.applications,
+            sessionDates: this.props.sessionDates
         })
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.applications !== this.state.applications) {
+            this.setState({
+                applications: this.props.applications,
+                sessionDates: this.props.sessionDates
+            })
+        }
+
+        if (prevState.sessionDates !== this.state.sessionDates) {
+            this.updateAvSData();
+        }
+    }
+
+    sessionDateCount = (sessionDate, allDates) => {
+        return allDates.filter(date => date === sessionDate)
+    }
+
+    updateAvSData = async () => {
+        let applications = this.state.applications;
+        let sessionDates = this.state.sessionDates;
+        let appliedDates = [];
+        let appliedDatesCount = [];
+        let skippedDates = [];
+        let skippedDatesCount = [];
+        let avsData = [{
+            title: {
+                applied: 'Applied',
+                skipped: 'Skipped'
+            },
+            data: {
+                applied: [],
+                skipped: []
+            }
+        }];
+        // change for loop i to 1 to omit first run
+        for (let i = 0; i < applications.length; i++) {
+            if (applications[i].apply_attempted) {
+                appliedDates.push(applications[i].session_date)
+            } else {
+                skippedDates.push(applications[i].session_date)
+            }
+        }
+
+        sessionDates.forEach(date => {
+            appliedDatesCount.push(this.sessionDateCount(date, appliedDates).length)
+        });
+
+        sessionDates.forEach(date => {
+            skippedDatesCount.push(this.sessionDateCount(date, skippedDates).length)
+        });
+        // change for loop i to 1 to omit first run
+        for (let i = 0; i < sessionDates.length; i++) {
+            avsData[0].data.applied.push({
+                "time": sessionDates[i],
+                "value": appliedDatesCount[i]
+            });
+
+            avsData[0].data.skipped.push({
+                "time": sessionDates[i],
+                "value": skippedDatesCount[i]
+            })
+        }
+
+        await this.setState({ avsData: avsData })
+
+    }
+
+
     render() {
-        return (
-            <canvas ref={this.chartRef} />
+        return(
+            <div className="col-8">
+                <LineChartAvSConstructor
+                    data={this.state.avsData[0].data}
+                    title={this.state.avsData[0].title}
+                    color="#3E517A"
+                />
+            </div>
+
         )
     }
 }
---
+++ jada-app/src/Components/LineChartAvSConstructor/LineChartAvSConstructor.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Chart from 'chart.js';
+import './lineChartAvS.css';
+
+class LineChartAvS extends React.Component {
+    constructor(props) {
+        super(props);
+        this.chartRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.produceChart()
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.data !== this.props.data) {
+            this.myChart.destroy();
+            this.produceChart();
+        }
+    }
+
+    produceChart = () => {
+        this.myChart = new Chart (this.chartRef.current, {
+            type: 'line',
+            options: {
+                scales: {
+                    xAxes: [
+                        {
+                            type: 'time',
+                            time: {
+                                unit: 'day'
+                            }
+                        }
+                    ],
+                    yAxes: [
+                        {
+                            ticks: {
+                                min: 0
+                            }
+                        }
+                    ]
+                }
+            },
+            data: {
+                labels: this.props.data.applied.map(d => d.time),
+                datasets: [{
+                    label: this.props.title.applied,
+                    data: this.props.data.applied.map(d => d.value),
+                    fill: 'none',
+                    backgroundColor: '#2ecc71',
+                    pointRadius: 2,
+                    borderColor: '#2ecc71',
+                    borderWidth: 1
+                },
+                    {
+                        label: this.props.title.skipped,
+                        data: this.props.data.skipped.map(d => d.value),
+                        fill: 'none',
+                        backgroundColor: '#e74c3c',
+                        pointRadius: 2,
+                        borderColor: '#e74c3c',
+                        borderWidth: 1
+                    }]
+            }
+        })
+    }
+
+    render() {
+        return (
+            <canvas ref={this.chartRef} />
+        )
+    }
+}
+
+export default LineChartAvS;
