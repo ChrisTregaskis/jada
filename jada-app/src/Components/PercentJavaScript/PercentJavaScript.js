14384029319f6475e630f0720c0873f32301d4da
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-09 18:51:11 +0100

    created dynamic js percent box using percentbox component

---
+++ jada-app/src/Components/PercentJavaScript/PercentJavaScript.js
@@ -0,0 +1,80 @@
+import React from "react";
+import PercentBoxConstructor from "../PercentBoxConstructor/PercentBoxConstructor";
+
+class PercentJavaScript extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            applications: {},
+            chartData: {},
+            percentFound: 0
+        }
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.applications !== this.state.applications) {
+            this.setState({
+                applications: this.props.applications
+            })
+            this.updatePercentFound()
+        }
+    }
+
+    updateChartData = (percent, keyWord) => {
+        let other = 100 - percent;
+        let data = {
+            labels: [keyWord, 'other'],
+            datasets: [{
+                data: [],
+                backgroundColor: ['#2ecc71']
+            }]
+        }
+        data.datasets[0].data.push(percent)
+        data.datasets[0].data.push(other)
+        this.setState({ chartData: data})
+    }
+
+    updatedKeyWordCount = (keyWord) => {
+        let applications = this.props.applications;
+        let allKeyWords = [];
+        let filteredKeyWord = [];
+
+        applications.forEach(applications => {
+            allKeyWords.push(...applications.found_dkw)
+        });
+
+        for (let i=0; i < allKeyWords.length; i++) {
+            if (allKeyWords[i] === keyWord || allKeyWords[i] === 'JS') {
+                filteredKeyWord.push(allKeyWords[i])
+            }
+        }
+
+        return filteredKeyWord.length;
+    }
+
+    updatePercentFound = async () => {
+        let applications = this.props.applications;
+        let total = applications.length;
+        let keyWord = 'JAVASCRIPT';
+        let totalJS = this.updatedKeyWordCount(keyWord)
+        let percent = ((totalJS / total) * 100).toFixed(0);
+        await this.setState({ percentFound: percent })
+        this.updateChartData(percent, keyWord)
+    }
+
+    render() {
+        return (
+            <div className="col-xl-3 percentageBox">
+                <p className="boxTitle d-flex justify-content-center">JAVASCRIPT:</p>
+                <PercentBoxConstructor
+                    data={this.state.chartData}
+                    percentFound={this.state.percentFound}
+                />
+            </div>
+        )
+    }
+
+}
+
+export default PercentJavaScript;
