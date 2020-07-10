31e5bc3c717f7aa7b2b22b6581bf28767712e723
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-10 10:37:18 +0100

    created basic tables page and added the page header component

---
+++ jada-app/src/Components/TablesPage/TablesPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './tablesPage.css';
+import PageHeader from "../PageHeader/PageHeader";
+
+class TablesPage extends React.Component {
+
+
+    render() {
+        return (
+            <div className="container">
+                <PageHeader />
+            </div>
+        );
+    }
+}
+
+export default TablesPage;
