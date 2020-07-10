4895e90e7db8a2cd6909e46575ab5288608816dc
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-10 10:36:52 +0100

    set up url routing for local host only

--- jada-app/src/index.js
+++ jada-app/src/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MainDashboard from "./Components/MainDashboard/MainDashboard";
+import TablesPage from "./Components/TablesPage/TablesPage";
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+class Routing extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={ MainDashboard } />
+                        <Route path="/tables" component={ TablesPage } />
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Routing />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
