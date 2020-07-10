2d9eb53015fbb282a899a0af413c916d58cf4b82
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-10 12:09:04 +0100

    updated lastUpdatedSession component and added an all applications button to go to applications table page

--- jada-app/src/Components/MainDashboard/MainDashboard.js
+++ jada-app/src/Components/MainDashboard/MainDashboard.js
@@ -14,6 +14,7 @@ import PercentPHP from "../Charts/PercentPHP/PercentPHP";
 import PercentNET from "../Charts/PercentNET/PercentNET";
 import PercentPython from "../Charts/PercentPython/PercentPython";
 import BarChartTop24 from "../Charts/BarChartTop24/BarChartTop24";
+import ButtonMain from "../Buttons/ButtonMain/ButtonMain";
 
 class MainDashboard extends React.Component {
     constructor(props) {
@@ -76,7 +77,14 @@ class MainDashboard extends React.Component {
         return(
             <div className="container">
                 <PageHeader/>
-                <LastUpdatedSession sessionDates={this.state.sessionDates} />
+                <div className="col-xl-12 d-flex justify-content-around">
+                    <LastUpdatedSession sessionDates={this.state.sessionDates} />
+                    <ButtonMain
+                        buttonText="ALL APPLICATIONS"
+                        cssClass="d-flex justify-content-center"
+                        location="http://localhost:3000/tables"
+                    />
+                </div>
                 <div className="col-xl-12 d-flex">
                     <LineChartAvS
                         applications={this.state.applications}
