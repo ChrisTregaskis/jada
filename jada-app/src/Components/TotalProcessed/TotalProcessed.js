70ac59eb77fb14f2e6851a39201aa051e2044b1a
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-10 12:07:14 +0100

    took handle click method off TotalProcessed as created dedicated button

--- jada-app/src/Components/TotalProcessed/TotalProcessed.js
+++ jada-app/src/Components/TotalProcessed/TotalProcessed.js
@@ -17,14 +17,10 @@ class TotalProcessed extends React.Component {
         }
     }
 
-    handleClick = () => {
-        window.location.replace('http://localhost:3000/tables')
-    }
-
     render() {
         return (
             <div className="col-xl-4">
-                <div className="totalProcessedBox" onClick={this.handleClick}>
+                <div className="totalProcessedBox">
                     <p className="boxTitle">TOTAL</p>
                     <p className="boxTitle">PROCESSED:</p>
                     <p className="totalProcessedFigure">
