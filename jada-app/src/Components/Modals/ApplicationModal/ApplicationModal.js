3c021771ddb681da30df963c7b057e05bfe6d40d
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-10 17:50:39 +0100

    created basic application model file with no functionality

---
+++ jada-app/src/Components/Modals/ApplicationModal/ApplicationModal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './applicationModal.css';
+
+class ApplicationModal extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ApplicationModal;
