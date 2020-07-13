d0bda37b91fc48cb45e7f11d3d43ae5f64beeb30
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-13 09:57:57 +0100

    updated positioning and style for modal as well as toggling active state

--- jada-app/src/Components/Modals/ApplicationModal/ApplicationModal.js
+++ jada-app/src/Components/Modals/ApplicationModal/ApplicationModal.js
@@ -5,12 +5,31 @@ class ApplicationModal extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            application: {},
+            modalClass: 'hidden'
+        }
     }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.modalActive !== this.props.modalActive) {
+            if (this.props.modalActive) {
+                this.setState({ modalClass: 'displayed' })
+            } else {
+                this.setState({ modalClass: 'hidden' })
+            }
+        }
+    }
+
+
+
     render() {
+        let modalClass = this.state.modalClass + ' applicationsModal'
         return (
-            <div>
-                
+            <div className={modalClass}>
+                <h1 className="title d-flex justify-content-end">JADA</h1>
+                <p>Job Title: Software Developer</p>
+
             </div>
         );
     }
