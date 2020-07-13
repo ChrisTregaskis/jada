38383bbbf1771968ee3f08ab2b64e7de03feabd6
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-13 12:43:27 +0100

    created display method and currently rendering apply atempted true successfully. Need to reformat data

--- jada-app/src/Components/Modals/ApplicationModal/ApplicationModal.js
+++ jada-app/src/Components/Modals/ApplicationModal/ApplicationModal.js
@@ -23,11 +23,77 @@ class ApplicationModal extends React.Component {
     }
 
     handleClick = () => {
-        if (this.state.modalActive) {
-            this.setState({ modalClass: 'displayed' })
-        } else {
-            this.setState({ modalClass: 'hidden' })
+        this.props.toggleModalActive();
+    }
+
+    reformatDate = (date) => {
+        let longDate = new Date(date).toString()
+        let longDateArr = longDate.split(" ")
+        let shortDateArr = [longDateArr[0], longDateArr[2], longDateArr[1], longDateArr[3]]
+        return shortDateArr.join(" ")
+    }
+
+    displayApplicationData = () => {
+        let application = this.props.applicationData;
+        let displayData = [];
+
+        if (application !== undefined) {
+            let date = this.reformatDate(application.session_date);
+
+            if (application.apply_attempted) {
+                displayData.push(
+                    <ul key={application.totalJobs_id}>
+                        <li key={application.totalJobs_id + 1} className="d-flex justify-content-center jobTitle">
+                            {application.job_title}
+                        </li>
+                        <li key={application.totalJobs_id + 2}>
+                            <span className="applicationDetail">Applied on:</span> {date}
+                        </li>
+                        <li key={application.totalJobs_id + 3}>
+                            <span className="applicationDetail">Applied at:</span> {application.session_time}
+                        </li>
+                        <li key={application.totalJobs_id + 4}>
+                            <span className="applicationDetail">TotalJobs id:</span> {application.totalJobs_id}
+                        </li>
+                        <li key={application.totalJobs_id + 5}>
+                            <span className="applicationDetail">Job reference:</span> {application.totalJobs_ref}
+                        </li>
+                        <li key={application.totalJobs_id + 6}>
+                            <span className="applicationDetail">Applied:</span> {application.apply_attempted}
+                        </li>
+                        <li key={application.totalJobs_id + 7}>
+                            <span className="applicationDetail">Interested:</span> {application.interested}
+                        </li>
+                        <li key={application.totalJobs_id + 8}>
+                            <span className="applicationDetail">Salary:</span> {application.salary}
+                        </li>
+                        <li key={application.totalJobs_id + 9}>
+                            <span className="applicationDetail">Job type:</span> {application.job_type}
+                        </li>
+                        <li key={application.totalJobs_id + 10}>
+                            <span className="applicationDetail">Job posted:</span> {application.job_posted}
+                        </li>
+                        <li key={application.totalJobs_id + 11}>
+                            <span className="applicationDetail">Job contact:</span> {application.job_contact}
+                        </li>
+                        <li key={application.totalJobs_id + 12}>
+                            <span className="applicationDetail">Job url:</span> {application.job_url}
+                        </li>
+                        <li key={application.totalJobs_id + 13}>
+                            <span className="applicationDetail">DKW found:</span> {application.found_dkw}
+                        </li>
+                        <li key={application.totalJobs_id + 14}>
+                            <span className="applicationDetail">UDKW found:</span> {application.found_udkw}
+                        </li>
+                        <li key={application.totalJobs_id + 15}>
+                            <span className="applicationDetail">Top24 found:</span> {application.found_top24}
+                        </li>
+                    </ul>
+                )
+            }
         }
+
+        return displayData
     }
 
     render() {
@@ -35,24 +101,10 @@ class ApplicationModal extends React.Component {
         return (
             <div className={modalClass}>
                 <h1 className="title d-flex justify-content-end">JADA</h1>
-                <ul>
-                    <li>Job title:</li>
-                    <li>Applied on:</li>
-                    <li>Applied at:</li>
-                    <li>TotalJobs id:</li>
-                    <li>Job reference:</li>
-                    <li>Applied:</li>
-                    <li>Interested:</li>
-                    <li>Salary:</li>
-                    <li>Job type:</li>
-                    <li>Job posted:</li>
-                    <li>Job contact:</li>
-                    <li>Job url:</li>
-                    <li>DKW found:</li>
-                    <li>UDKW found:</li>
-                    <li>Top24 found:</li>
-                </ul>
-                <div className="modalButtonBox d-flex justify-content-center">
+
+                {this.displayApplicationData()}
+
+                <div className="modalButtonBox">
                     <ButtonMainToggle
                         buttonText="DONE"
                         handleClick={this.handleClick}
