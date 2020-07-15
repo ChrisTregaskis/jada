9ce28ea05f37b4cd16785426626ee38f02a3ff01
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-15 20:29:30 +0100

    added check authorisation to delete route before one can delete a user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UsersController = require('../controllers/users');
+const checkAuth = require('../middleware/check-auth');
 
 router.post('/signup', UsersController.users_sign_up);
 router.post('/login', UsersController.users_login);
-router.delete('/:userId', UsersController.users_delete_user);
+router.delete('/:userId', checkAuth, UsersController.users_delete_user);
 
 module.exports = router;
