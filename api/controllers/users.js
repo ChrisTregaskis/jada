2af6b550545a4aeae0d2a57e36ef47b75d3410d8
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-15 17:17:43 +0100

    added route and controller functionality to delete a user

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -4,37 +4,37 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.users_sign_up = (req, res, next) => {
-  User.find({ email: req.body.email })
-      .exec()
-      .then(user => {
-          if (user.length >= 1) {
-              return res.status(422).json({ message: 'Email exists' });
-          } else {
-              bcrypt.hash(req.body.password, 10, (err, hash) => {
-                  if (err) {
-                      return res.status(500).json({ error: err });
-                  } else {
-                      const user = new User({
-                          _id: new mongoose.Types.ObjectId(),
-                          email: req.body.email,
-                          password: hash
-                      });
-                      user
-                          .save()
-                          .then(result => {
-                              console.log(result)
-                              res.status(201).json({ message: 'User created' })
-                          })
-                          .catch(err => {
-                              res.status(500).json({ error: err })
-                          })
-                  }
-              })
-          }
-      })
-      .catch(err => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                  return res.status(422).json({ message: 'Email exists' });
+                } else {
+                    bcrypt.hash(req.body.password, 10, (err, hash) => {
+                        if (err) {
+                            return res.status(500).json({ error: err });
+                        } else {
+                            const user = new User({
+                                _id: new mongoose.Types.ObjectId(),
+                                email: req.body.email,
+                                password: hash
+                            });
+                            user
+                                .save()
+                                .then(result => {
+                                  console.log(result)
+                                  res.status(201).json({ message: 'User created' })
+                                })
+                                .catch(err => {
+                                  res.status(500).json({ error: err })
+                                })
+                      }
+                  })
+            }
+        })
+        .catch(err => {
           res.status(500).json({ error: err })
-      })
+        })
 };
 
 exports.users_login = (req, res, next) => {
@@ -67,4 +67,15 @@ exports.users_login = (req, res, next) => {
         .catch(err => {
             res.status(500).json({ error: err })
         })
+};
+
+exports.users_delete_user = (req, res, next) => {
+    User.deleteOne({ _id: req.params.userId })
+      .exec()
+      .then(result => {
+          res.status(200).json({ message: 'User deleted' });
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      });
 };
--- api/routes/user.js
+++ api/routes/user.js
@@ -4,5 +4,6 @@ const UsersController = require('../controllers/users');
 
 router.post('/signup', UsersController.users_sign_up);
 router.post('/login', UsersController.users_login);
+router.delete('/:userId', UsersController.users_delete_user);
 
 module.exports = router;
