5cb45c1775bba6acd8244be46b588cf2c7391425
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-15 16:45:44 +0100

    added user login functionality, and successfully responding with a token

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -36,3 +36,35 @@ exports.users_sign_up = (req, res, next) => {
           res.status(500).json({ error: err })
       })
 };
+
+exports.users_login = (req, res, next) => {
+    User.find({ email: req.body.email })
+        .exec()
+        .then(user => {
+            if (user.length < 1) {
+                return res.status(401).json({ message: 'Auth failed' });
+            }
+
+            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({ message: 'Auth failed' })
+                }
+
+                if (result) {
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    }, process.env.JWT_KEY, { expiresIn: "1h" })
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        token: token
+                    });
+                }
+
+                res.status(401).json({ message: 'Auth failed' })
+            })
+        })
+        .catch(err => {
+            res.status(500).json({ error: err })
+        })
+};
--- api/routes/user.js
+++ api/routes/user.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const UsersController = require('../controllers/users');
 
 router.post('/signup', UsersController.users_sign_up);
+router.post('/login', UsersController.users_login);
 
 module.exports = router;
