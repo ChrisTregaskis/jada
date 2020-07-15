9d77956389cb5b4aeda0306270e24b0a6f335c5c
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-15 16:13:54 +0100

    added functionality to add a user to db, using password hashing

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -4,7 +4,35 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 exports.users_sign_up = (req, res, next) => {
-  return res.status(200).json({
-      message: 'Controller working!'
-  })
+  User.find({ email: req.body.email })
+      .exec()
+      .then(user => {
+          if (user.length >= 1) {
+              return res.status(422).json({ message: 'Email exists' });
+          } else {
+              bcrypt.hash(req.body.password, 10, (err, hash) => {
+                  if (err) {
+                      return res.status(500).json({ error: err });
+                  } else {
+                      const user = new User({
+                          _id: new mongoose.Types.ObjectId(),
+                          email: req.body.email,
+                          password: hash
+                      });
+                      user
+                          .save()
+                          .then(result => {
+                              console.log(result)
+                              res.status(201).json({ message: 'User created' })
+                          })
+                          .catch(err => {
+                              res.status(500).json({ error: err })
+                          })
+                  }
+              })
+          }
+      })
+      .catch(err => {
+          res.status(500).json({ error: err })
+      })
 };
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const UsersController = require('../controllers/users');
 
-router.get('/signup', UsersController.users_sign_up);
+router.post('/signup', UsersController.users_sign_up);
 
 module.exports = router;
