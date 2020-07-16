db5ca291389641cf1d8b74355713e73de6c11092
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-16 16:37:39 +0100

    added ability to get all applications via a user id to api

--- api/routes/applications.js
+++ api/routes/applications.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const applicationsController = require('../controllers/applicationsController');
 
 router.get('/', applicationsController.get_all_applications);
-router.post('/', applicationsController.log_application);
 router.get('/:applicationId', applicationsController.get_application);
 router.get('/session/:sessionId', applicationsController.get_by_session_id);
+router.get('/user/:userId', applicationsController.get_by_user_id);
+router.post('/', applicationsController.log_application);
 router.delete('/:applicationId', applicationsController.delete_application);
 router.delete('/', applicationsController.delete_applications);
 
