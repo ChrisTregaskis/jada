e35643b7a4e7f054d4a465faa1b07856662070d1
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-17 11:19:17 +0100

    created initial login page component

---
+++ jada-app/src/Components/Pages/LoginPage/LoginPage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './loginPage.css';
+
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+        return (
+            <div className="container-fluid loginPage">
+
+            </div>
+        );
+    }
+}
+
+export default LoginPage;
