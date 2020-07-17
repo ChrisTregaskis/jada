c1e53015879a32c7778c324af38ba4ed34be9cb1
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-17 11:36:17 +0100

    created and positioned plain login box

---
+++ jada-app/src/Components/StandAloneComponents/LoginBox/LoginBox.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './loginBox.css';
+
+class LoginBox extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+        return (
+            <div className="loginBox">
+
+            </div>
+        );
+    }
+}
+
+export default LoginBox;
