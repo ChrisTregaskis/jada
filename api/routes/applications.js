e60f46789e369a8e0f06e06c88e70fff5e016ad9
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-17 08:42:14 +0100

    removed delete all route and controller method. This route deleted all where test application was true

--- api/routes/applications.js
+++ api/routes/applications.js
@@ -9,6 +9,5 @@ router.get('/user/:userId', applicationsController.get_by_user_id);
 router.post('/', applicationsController.log_application);
 router.delete('/:applicationId', applicationsController.delete_application);
 router.delete('/user/:userId', applicationsController.delete_applications_by_user_id);
-router.delete('/', applicationsController.delete_applications);
 
 module.exports = router;
