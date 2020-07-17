91ee47bc77f92b0006a6f59f12d4f20f7fd3695e
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-17 12:40:39 +0100

    created form and styled appropriately

--- jada-app/src/Components/StandAloneComponents/LoginBox/LoginBox.js
+++ jada-app/src/Components/StandAloneComponents/LoginBox/LoginBox.js
@@ -11,7 +11,23 @@ class LoginBox extends React.Component {
     render() {
         return (
             <div className="loginBox">
-
+                <h2 className="title d-flex justify-content-end">JADA</h2>
+                <div>
+                    <form action="" className="log-in" autoComplete="off">
+                        <h4>Welcome</h4>
+                        <p>Welcome back! Log in to your account to view JADA results:</p>
+                        <div className="floating-label">
+                            <input placeholder="Email" type="text" name="email" id="email" autoComplete="off"/>
+                                <label htmlFor="email">Email:</label>
+                        </div>
+                        <div className="floating-label">
+                            <input placeholder="Password" type="password" name="password" id="password"
+                                   autoComplete="off"/>
+                                <label htmlFor="password">Password:</label>
+                        </div>
+                        <button type="submit" onClick="return false;">Log in</button>
+                    </form>
+                </div>
             </div>
         );
     }
