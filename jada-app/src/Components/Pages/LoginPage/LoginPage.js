8f5e83eb1fb559d09930cc6c1a6bb1544a983e8b
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-21 11:16:56 +0100

    added a toggle method to update active form view

--- jada-app/src/Components/Pages/LoginPage/LoginPage.js
+++ jada-app/src/Components/Pages/LoginPage/LoginPage.js
@@ -3,13 +3,28 @@ import './loginPage.css';
 import LoginBox from "../../StandAloneComponents/LoginBox/LoginBox";
 
 class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            activeForm: 'LogIn'
+        }
+    }
 
+    toggleForm = () => {
+        if (this.state.activeForm === 'LogIn') {
+            this.setState({ activeForm: 'SignUp' })
+        } else if (this.state.activeForm === 'SignUp') {
+            this.setState({ activeForm: 'LogIn' })
+        }
+    }
 
     render() {
         return (
             <div className="container-fluid loginPage">
-                <LoginBox />
+                <LoginBox
+                    toggleForm={this.toggleForm}
+                />
             </div>
         );
     }
