51ca232e35b752fa233085b0bb7ddf70dd35d968
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-25 10:14:20 +0100

    refactored get applications by user id controller into its own file

--- api/routes/applications.js
+++ api/routes/applications.js
@@ -4,6 +4,7 @@ const checkAuth = require('../middleware/check-auth');
 const getAllApplicationsController = require('../controllers/applications/getAllApplicationsController');
 const getApplicationsBySessionIdController =
     require('../controllers/applications/getApplicationsBySessionIdController');
+const getApplicationsByUserIdController = require('../controllers/applications/getApplicationsByUserIdController');
 
 
 const applicationsController = require('../controllers/applicationsController');
@@ -11,7 +12,7 @@ const applicationsController = require('../controllers/applicationsController');
 router.get('/', checkAuth, getAllApplicationsController.get_all_applications);
 router.get('/:applicationId', checkAuth, applicationsController.get_application);
 router.get('/session/:sessionId', checkAuth, getApplicationsBySessionIdController.get_by_session_id);
-router.get('/user/:userId', checkAuth, applicationsController.get_by_user_id);
+router.get('/user/:userId', checkAuth, getApplicationsByUserIdController.get_by_user_id);
 router.post('/', applicationsController.log_application);
 router.delete('/:applicationId', checkAuth, applicationsController.delete_application);
 router.delete('/user/:userId', checkAuth, applicationsController.delete_applications_by_user_id);
