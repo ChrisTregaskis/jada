059ff554f3701b8569a64333eaf3babae319c226
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-25 10:10:13 +0100

    refactored get applications by session id controller into its own file

---
+++ api/controllers/applications/getApplicationsBySessionIdController.js
@@ -0,0 +1,55 @@
+const Application = require('../../models/applicationModel')
+
+exports.get_by_session_id = (req, res, next) => {
+    let sessionId = req.params.sessionId
+    Application
+        .find({ "session_id" : sessionId })
+        .exec()
+        .then(docs => {
+            const response = {
+                status: 200,
+                count: docs.length,
+                applications: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        user_id: doc.user_id,
+                        session_id: doc.session_id,
+                        session_date: doc.session_date,
+                        session_time: doc.session_time,
+                        job_title: doc.job_title,
+                        totalJobs_id: doc.totalJobs_id,
+                        apply_attempted: doc.apply_attempted,
+                        interested: doc.interested,
+                        salary: doc.salary,
+                        company: doc.company,
+                        job_type: doc.job_type,
+                        job_posted: doc.job_posted,
+                        location: doc.location,
+                        job_url: doc.job_url,
+                        job_contact: doc.job_contact,
+                        totalJobs_ref: doc.totalJobs_ref,
+                        found_dkw: doc.found_dkw,
+                        found_udkw: doc.found_udkw,
+                        found_top24: doc.found_top24
+
+                    }
+                })
+            }
+
+            if (docs.length > 0) {
+                res.status(200).json({response})
+            } else {
+                res.status(404).json({
+                    status: 404,
+                    message: 'no data in db'
+                })
+            }
+
+        })
+        .catch(err => {
+            res.status(500).json({
+                status: 500,
+                error: err
+            });
+        })
+};
--- api/routes/applications.js
+++ api/routes/applications.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const getAllApplicationsController = require('../controllers/applications/getAllApplicationsController');
+const getApplicationsBySessionIdController =
+    require('../controllers/applications/getApplicationsBySessionIdController');
+
 
 const applicationsController = require('../controllers/applicationsController');
 
 router.get('/', checkAuth, getAllApplicationsController.get_all_applications);
 router.get('/:applicationId', checkAuth, applicationsController.get_application);
-router.get('/session/:sessionId', checkAuth, applicationsController.get_by_session_id);
+router.get('/session/:sessionId', checkAuth, getApplicationsBySessionIdController.get_by_session_id);
 router.get('/user/:userId', checkAuth, applicationsController.get_by_user_id);
 router.post('/', applicationsController.log_application);
 router.delete('/:applicationId', checkAuth, applicationsController.delete_application);
