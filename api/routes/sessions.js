971b9c956b9710a863fbfcf195bff4fda62ea029
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-25 10:43:36 +0100

    refactored get session controller into its own file

--- api/routes/sessions.js
+++ api/routes/sessions.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const getAllSessionsController = require('../controllers/sessions/getAllSessionsController');
+const getSessionController = require('../controllers/sessions/getSessionController');
 
 const sessionsController = require('../controllers/sessionsController');
 
 router.get('/', checkAuth, getAllSessionsController.get_all_sessions);
 router.post('/', sessionsController.log_session);
-router.get('/:sessionId', checkAuth, sessionsController.get_session);
+router.get('/:sessionId', checkAuth, getSessionController.get_session);
 
 module.exports = router;
