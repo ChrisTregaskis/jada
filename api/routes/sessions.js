e5a14a6730730c0e7ab59296ec1c34f1218a62b8
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-25 10:45:59 +0100

    refactored log session to db controller to its own file

--- api/routes/sessions.js
+++ api/routes/sessions.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const getAllSessionsController = require('../controllers/sessions/getAllSessionsController');
 const getSessionController = require('../controllers/sessions/getSessionController');
-
-const sessionsController = require('../controllers/sessionsController');
+const postSessionToDBController = require('../controllers/sessions/postSessionToDBController');
 
 router.get('/', checkAuth, getAllSessionsController.get_all_sessions);
-router.post('/', sessionsController.log_session);
+router.post('/', postSessionToDBController.log_session);
 router.get('/:sessionId', checkAuth, getSessionController.get_session);
 
 module.exports = router;
