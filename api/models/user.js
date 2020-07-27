20e95f5f7eeb2bb9b825ee37faf493128f8fd2a0
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-27 10:49:58 +0100

    updated regex

--- api/models/user.js
+++ api/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const emailRegEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
