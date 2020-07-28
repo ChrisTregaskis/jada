12471748309b32dede3d01a47495efcff4d8e0fa
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-07-28 11:23:49 +0100

    taking the encrypted totalJobs pass, decrypting and inputing on browser

--- api/controllers/sessions/totalJobsLogIn/totalJobsLogInActions.js
+++ api/controllers/sessions/totalJobsLogIn/totalJobsLogInActions.js
@@ -1,4 +1,7 @@
-const WebDriver = require('selenium-webdriver')
+const aesjs = require('aes-js');
+const voltPackage = require('../../../volt');
+const volt = voltPackage.open_volt();
+const WebDriver = require('selenium-webdriver');
 const driver = new WebDriver.Builder().forBrowser('chrome').build();
 
 exports.navigate_to_website = async function() {
@@ -19,9 +22,13 @@ exports.navigate_to_loginPage = async function() {
 };
 
 exports.jobSeeker_login = async function(emailLogIn, passwordLogIn) {
+    const encryptedBytes = aesjs.utils.hex.toBytes(passwordLogIn);
+    const aesCtr = new aesjs.ModeOfOperation.ctr(volt.AES_KEY, new aesjs.Counter(5));
+    const decryptedBytes = aesCtr.decrypt(encryptedBytes);
+    const pass = aesjs.utils.utf8.fromBytes(decryptedBytes);
     try {
         await driver.findElement({ id: 'Form_Email' }).sendKeys(emailLogIn);
-        await driver.findElement({ id: 'Form_Password' }).sendKeys(passwordLogIn);
+        await driver.findElement({ id: 'Form_Password' }).sendKeys(pass);
         await driver.findElement({ id: 'Form_RememberMe' }).click();
         await driver.findElement({ id: 'btnLogin' }).click();
         await driver.wait(WebDriver.until.elementLocated({ id: 'search-button' }), 20000);
--- api/controllers/sessions/totalJobsLogIn/totalJobsLogInController.js
+++ api/controllers/sessions/totalJobsLogIn/totalJobsLogInController.js
@@ -1,9 +1,10 @@
 const { navigate_to_website, navigate_to_loginPage, jobSeeker_login } = require('./totalJobsLogInActions');
 const { validate_email } = require('../../../Entities/validationEntity')
+const aesjs = require('aes-js');
 
 exports.totalJobs_logIn = async (req, res, next) => {
-    const requestEmail = req.body.totalJobs_email;
-    const requestPass = req.body.totalJobs_hashPass;
+    const requestEmail = req.body.email;
+    const requestPass = req.body.encPass;
     if (typeof requestEmail !== "string" || typeof requestPass !== "string") {
         await res.status(400).json({
             success: false,
@@ -12,15 +13,15 @@ exports.totalJobs_logIn = async (req, res, next) => {
         return
     }
 
-    const totalJobsEmail = requestEmail.trim();
-    const totalJobsHashPass = requestPass.trim();
-    if (totalJobsEmail.length < 1 || totalJobsHashPass < 1) {
+    const email = requestEmail.trim();
+    const encPass = requestPass.trim();
+    if (email.length < 1 || encPass < 1) {
         await res.status(400).json({
             success: false,
             message: 'Email or password field empty'
         })
         return
-    } else if (validate_email(totalJobsEmail) === 'invalid email') {
+    } else if (validate_email(email) === 'invalid email') {
         await res.status(400).json({
             success: false,
             message: 'Invalid email'
@@ -28,14 +29,32 @@ exports.totalJobs_logIn = async (req, res, next) => {
         return
     }
 
+    var key = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+        29, 30, 31];
+    // Convert text to bytes
+    var text = 'gSpJ2biL$XDHwEQ';
+    var textBytes = aesjs.utils.utf8.toBytes(text);
+
+    // The counter is optional, and if omitted will begin at 1
+    var aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(5));
+    var encryptedBytes = aesCtr.encrypt(textBytes);
+
+    // To print or store the binary data, you may convert it to hex
+    var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+    console.log(encryptedHex);
+    // "a338eda3874ed884b6199150d36f49988c90f5c47fe7792b0cf8c7f77eeffd87
+    //  ea145b73e82aefcf2076f881c88879e4e25b1d7b24ba2788"
+
     try {
         await navigate_to_website();
         await navigate_to_loginPage();
-        await jobSeeker_login(totalJobsEmail, totalJobsHashPass);
+        await jobSeeker_login(email, encPass);
         await res.status(200).json({
             success: true,
             message: 'Successfully logged into totalJobs account',
-            email: totalJobsEmail
+            email: email,
+            decrypted: decryptedText
         })
     } catch (err) {
         if (err.name === 'TimeoutError') {
@@ -49,5 +68,3 @@ exports.totalJobs_logIn = async (req, res, next) => {
     }
 
 }
-
-
