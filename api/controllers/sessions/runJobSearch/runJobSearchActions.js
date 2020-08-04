1b4b818a7165d140edf13f076d2525dbb37f957d
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-08-04 19:27:45 +0100

    clearing inputs before entering data to avoid duplication

--- api/controllers/sessions/runJobSearch/runJobSearchActions.js
+++ api/controllers/sessions/runJobSearch/runJobSearchActions.js
@@ -3,12 +3,14 @@ const driver = localWebDriver.get_driver();
 
 exports.enter_job_title = async (jobTitle) => {
     try {
+        await driver.findElement({ id: 'keywords' }).clear();
         await driver.findElement({ id: 'keywords' }).sendKeys(jobTitle);
     } catch (err) { throw err }
 }
 
 exports.enter_location = async (location) => {
     try {
+        await driver.findElement({ id: 'location' }).clear();
         await driver.findElement({ id: 'location' }).sendKeys(location);
     } catch (err) { throw err }
 }
