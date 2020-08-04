ab9d77231b98ce04026f7f803c2be339c37536fc
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-08-04 18:57:08 +0100

    refactored code to use instance of driver and fillling search params on page. NOT validated

---
+++ api/controllers/sessions/runJobSearch/runJobSearchActions.js
@@ -0,0 +1,20 @@
+const localWebDriver = require('../webDriver');
+const driver = localWebDriver.get_driver();
+
+exports.enter_job_title = async (jobTitle) => {
+    try {
+        await driver.findElement({ id: 'keywords' }).sendKeys(jobTitle);
+    } catch (err) { throw err }
+}
+
+exports.enter_location = async (location) => {
+    try {
+        await driver.findElement({ id: 'location' }).sendKeys(location);
+    } catch (err) { throw err }
+}
+
+exports.enter_radius = async (radius) => {
+    try {
+        await driver.findElement({ id: 'LocationType' }).sendKeys(radius);
+    } catch (err) { throw err }
+}
--- api/controllers/sessions/runJobSearch/runJobSearchController.js
+++ api/controllers/sessions/runJobSearch/runJobSearchController.js
@@ -1,5 +1,19 @@
-
+const { enter_job_title, enter_location, enter_radius } = require('../runJobSearch/runJobSearchActions');
 
 exports.enter_search = async (req, res, next) => {
-    res.status(200).json({ message: 'I hear ya!'})
+    const reqJobTitle = req.body.job_title;
+    const reqLocation = req.body.location;
+    const reqRadius = req.body.radius;
+
+    // validate and sanitize data
+
+    try {
+        await enter_job_title(reqJobTitle)
+        await enter_location(reqLocation)
+        await enter_radius(reqRadius)
+        await res.status(200).json({ message: 'I hear ya!'})
+    } catch (err) {
+        await res.status(500).json({ error: err })
+    }
+
 }
