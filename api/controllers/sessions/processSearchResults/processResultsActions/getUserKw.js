f80feb00b4763b2cb46f152dda166e24b3a76740
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-08-07 12:59:35 +0100

    added nextBtnStatus, createSessionDetail and getUserKw methods in their own respective files

---
+++ api/controllers/sessions/processSearchResults/processResultsActions/getUserKw.js
@@ -0,0 +1,14 @@
+const User = require('../../../../models/user');
+
+exports.get_user_kw = async (id) => {
+    let userData = await User.findById(id)
+    let userPreferences = {};
+    try {
+        userPreferences = {
+            dkw: userData.preferences.dkw,
+            udkw: userData.preferences.udkw,
+            ikw: userData.preferences.ikw
+        }
+        return userPreferences
+    } catch (err) { throw err }
+}
---
+++ api/controllers/sessions/processSearchResults/processResultsActions/nextBtnStatus.js
@@ -0,0 +1,23 @@
+const localWebDriver = require('../../webDriver');
+const driver = localWebDriver.get_driver();
+
+exports.next_btn_status = async () => {
+    try {
+        let nextBtnElement;
+        nextBtnElement = await driver.findElements({ css: '.pagination .next' });
+
+        if (nextBtnElement.length > 0) {
+            nextBtnElement = await driver.findElement({ css: '.pagination .next' });
+            let nextBtnClasses = await nextBtnElement.getAttribute('class');
+            let explodedBtnClasses = nextBtnClasses.split(" ");
+            let disabledNextBtn = explodedBtnClasses.includes('disabled')
+            return !disabledNextBtn;
+        } else {
+            return false
+        }
+
+    } catch (err) {
+        console.log(err)
+        return 'error'
+    }
+}
