86e3dfce5922780c06cbd760c16dfa485ebf7aec
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-08-07 19:12:31 +0100

    added grab job salary method and handling error and if element doesn't exist

--- api/controllers/sessions/processSearchResults/processResultsActions/grabJobData.js
+++ api/controllers/sessions/processSearchResults/processResultsActions/grabJobData.js
@@ -1,14 +1,18 @@
 const { grab_job_description } = require('./grabJobData/grabJobDescription');
+const { grab_job_salary } = require('./grabJobData/grabJobSalary');
 
 exports.grab_all_job_data = async () => {
     try {
         let jobDescription = await grab_job_description();
         if (!(jobDescription.success)) { return jobDescription }
 
+        let salary = await grab_job_salary();
+        if (salary.error) { return salary }
 
         return {
             success: true,
-            job_description: jobDescription.data
+            job_description: jobDescription.data,
+            salary: salary
         }
     } catch (err) {
         console.log(err)
