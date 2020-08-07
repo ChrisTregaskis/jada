92e2dfcd9af49f122916ddf63014c0496b437628
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-08-07 19:52:03 +0100

    created and added method to grab job title from job add

--- api/controllers/sessions/processSearchResults/processResultsActions/grabJobData.js
+++ api/controllers/sessions/processSearchResults/processResultsActions/grabJobData.js
@@ -1,10 +1,14 @@
 const { grab_job_description } = require('./grabJobData/grabJobDescription');
 const { grab_job_salary } = require('./grabJobData/grabJobSalary');
+const { grab_job_title } = require('./grabJobData/grabJobTitle');
 
 exports.grab_all_job_data = async () => {
     try {
         let jobDesc = await grab_job_description();
         if (!(jobDesc.success)) { return jobDesc }
+
+        let jobTitle = await grab_job_title();
+        if (!(jobTitle.success)) { return jobTitle }
 
         let salary = await grab_job_salary();
         if (salary.error) { return salary }
@@ -12,7 +16,9 @@ exports.grab_all_job_data = async () => {
         return {
             success: true,
             job_desc: jobDesc.data,
+            job_title: jobTitle.data,
             salary: salary
+
         }
     } catch (err) {
         console.log(err)
