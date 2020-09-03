c3658b5bc1b4f2f53d61faa328e7bda08aec09b9
ChrisTregaskis <c.tregaskis@gmail.com>
2020-09-03 16:25:26 +0100

    changed test code to test desirability job type methods

--- api/controllers/sessions/sessionTestController.js
+++ api/controllers/sessions/sessionTestController.js
@@ -1,20 +1,28 @@
-const { apply_to_job } = require('../sessions/processSearchResults/processResultsActions/applyToJob');
+const { grab_all_job_data } = require('./processSearchResults/processResultsActions/grabJobData');
+const { check_desirability_job_type } = require('./processSearchResults/processResultsActions/checkDesirabilityByPreference')
+
 const localWebDriver = require('../sessions/webDriver');
 const driver = localWebDriver.get_driver();
 
 exports.run_test = async (req, res, next) => {
-    const url = 'https://www.totaljobs.com/job/software-developer/premier-it-job90603037';
+    const url = 'https://www.totaljobs.com/job/administrative-officer/guidant-global-job90645387';
     await driver.get(url);
 
-    let applyToJob = await apply_to_job();
-
-    if (applyToJob.error) {
-        await res.status(500).json({ error: applyToJob.error })
-    } else if (applyToJob.success) {
-        await res.status(200).json({
-            success: true,
-            message: 'session test controller',
-            applyToJob: applyToJob
-        })
+    let jobData = await grab_all_job_data('90645387', 'https://www.totaljobs.com/job/administrative-officer/guidant-global-job90645387');
+    if (!(jobData.success)) {
+        return {
+            success: false,
+            message: 'System error grabbing job data'
+        }
     }
+
+    let applicationJobType = jobData.job_info.job_type;
+    let desiredJobType = await check_desirability_job_type(applicationJobType, '5f328a56e941284b21b2da3f')
+
+    return await res.status(200).json({
+        success: true,
+        message: 'session test controller',
+        desired_job_type: desiredJobType,
+        job_data_success: jobData.success
+    })
 }
