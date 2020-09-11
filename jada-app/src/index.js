aa1f2bb55ac3502422027f39547f4670862046af
ChrisTregaskis <c.tregaskis@gmail.com>
2020-09-11 16:42:58 +0100

    added apply page route

--- jada-app/src/index.js
+++ jada-app/src/index.js
@@ -5,6 +5,7 @@ import MainDashboard from "./Components/Pages/MainDashboard/MainDashboard";
 import TablesPage from "./Components/Pages/TablesPage/TablesPage";
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import LoginPage from "./Components/Pages/LoginPage/LoginPage";
+import ApplyPage from "./Components/Pages/ApplyPage/ApplyPage";
 
 class Routing extends React.Component {
     render() {
@@ -15,6 +16,7 @@ class Routing extends React.Component {
                         <Route exact path="/" component={ LoginPage } />
                         <Route path="/dashboard" component={ MainDashboard } />
                         <Route path="/tables" component={ TablesPage } />
+                        <Route path="/apply" component={ ApplyPage } />
                     </Switch>
                 </div>
             </Router>
