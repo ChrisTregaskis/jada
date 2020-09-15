b003bd4c01ff4f0236703ee07bd462326372d02f
ChrisTregaskis <c.tregaskis@gmail.com>
2020-09-15 20:33:33 +0100

    created component file for displaying key words, nothing in it yet

---
+++ jada-app/src/Components/StandAloneComponents/UserKeyWords/KeyWordsContructor/KeyWordsConstructor.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './keyWordsConstructor.css';
+
+class KeyWordsConstructor extends React.Component {
+
+    render() {
+        return (
+            <div className="keyWordsBox">
+
+            </div>
+        );
+    }
+}
+
+export default KeyWordsConstructor;
