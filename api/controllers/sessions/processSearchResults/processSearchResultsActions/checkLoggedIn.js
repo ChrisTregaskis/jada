a1643ab93bb233e793285dcec4307d5ef6d09043
ChrisTregaskis <c.tregaskis@gmail.com>
2020-09-29 11:59:46 +0100

    amended driver to wait for class name to load

--- api/controllers/sessions/processSearchResults/processSearchResultsActions/checkLoggedIn.js
+++ api/controllers/sessions/processSearchResults/processSearchResultsActions/checkLoggedIn.js
@@ -1,14 +1,25 @@
+const WebDriver = require('selenium-webdriver');
 const localWebDriver = require('../../webDriver');
 const driver = localWebDriver.get_driver();
 
 exports.check_logged_in = async () => {
     try {
+        let loggedInClass = await driver.wait(WebDriver.until.elementLocated({
+            className: "navbar-header logged-in"
+        }), 20000)
         let navBarWebElement = await driver.findElement({ id: "navbar-header" });
         let navBarClass = await navBarWebElement.getAttribute('class');
         let navBarClasses = navBarClass.split(" ");
-        return navBarClasses.includes("logged-in")
+        let loggedIn = navBarClasses.includes("logged-in")
+        return {
+            success: loggedIn
+        }
+
     } catch (err) {
         console.log(err)
-        return false
+        return {
+            success: false,
+            error: err
+        }
     }
 }
